adc14026e3fc9c60829563551ea6f5985f13df93
Blair Hall <36650103+BlairDHall@users.noreply.github.com>
2019-11-10 11:34:14 +1300

    First version, all tests working in py2.7

---
+++ docs/index.rst
@@ -0,0 +1,19 @@
+==============
+Quantity-Value
+==============
+
+
+Release Notes
+-------------
+.. toctree::
+   :maxdepth: 2
+
+    License <license>
+    Authors <authors>
+    Change Log <changelog>
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
--- quantity_value/__init__.py
+++ quantity_value/__init__.py
@@ -7,6 +7,8 @@ from .quantity_value import *
 from .unit_system import *
 from .context import *
 
+#----------------------------------------------------------------------------
+
 version = "0.1.0.dev0"
 copyright = """Copyright (c) 2019, \
 Measurement Standards Laboratory of New Zealand"""
