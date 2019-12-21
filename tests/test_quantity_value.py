3786073dbad268ad5d5fa66f6fdb904516ce0a7e
Blair Hall <36650103+BlairDHall@users.noreply.github.com>
2019-12-22 07:38:54 +1300

    Tests changed to adapt to ValueUnit

--- tests/test_quantity_value.py
+++ tests/test_quantity_value.py
@@ -20,11 +20,11 @@ class TestQuantityValue(unittest.TestCase):
         metre = SIUnits.unit(Length,'metre','m')
         
         x = 1.234
-        qv = QuantityValue(x,metre)
+        vu = ValueUnit(x,metre)
         
-        self.assertTrue( isinstance(qv,QuantityValue) )
-        self.assertTrue( quantity(qv) is metre )
-        self.assertAlmostEqual( value(qv), x, 15 )       
+        self.assertTrue( isinstance(vu,ValueUnit) )
+        self.assertTrue( unit(vu) is metre )
+        self.assertAlmostEqual( value(vu), x, 15 )       
  
     def test_simple_addition_subtraction(self):
         
@@ -35,45 +35,45 @@ class TestQuantityValue(unittest.TestCase):
         x1 = 1.2 
         x2 = 3.4 
         
-        qv1 = QuantityValue(x1,metre)
-        qv2 = QuantityValue(x2,metre)
+        qv1 = ValueUnit(x1,metre)
+        qv2 = ValueUnit(x2,metre)
         
-        qv = qv1 + qv2 
-        self.assertAlmostEqual( qv.x, x1 + x2, 15 )       
-        self.assertTrue( qv.q is metre )
+        vu = qv1 + qv2 
+        self.assertAlmostEqual( vu.x, x1 + x2, 15 )       
+        self.assertTrue( vu.u is metre )
   
-        qv = qv1 - qv2 
-        self.assertAlmostEqual( qv.x, x1 - x2, 15 )       
-        self.assertTrue( qv.q is metre )
+        vu = qv1 - qv2 
+        self.assertAlmostEqual( vu.x, x1 - x2, 15 )       
+        self.assertTrue( vu.u is metre )
         
         # When the units have different prefixes
-        qv2 = QuantityValue(x2,centi(metre))
-        qv = qv1 + qv2 
+        qv2 = ValueUnit(x2,centi(metre))
+        vu = qv1 + qv2 
         # The result will be expressed in the lesser of the units 
-        self.assertAlmostEqual( value(qv), x1*100 + x2, 15 )       
-        self.assertTrue( quantity(qv) is centi(metre) )
+        self.assertAlmostEqual( value(vu), x1*100 + x2, 15 )       
+        self.assertTrue( unit(vu) is centi(metre) )
 
-        qv = qv1 - qv2 
-        self.assertAlmostEqual( qv.x, x1*100 - x2, 15 )       
-        self.assertTrue( qv.q is centi(metre) )
+        vu = qv1 - qv2 
+        self.assertAlmostEqual( vu.x, x1*100 - x2, 15 )       
+        self.assertTrue( vu.u is centi(metre) )
 
         # Illegal case 
         Imperial = UnitSystem("Imperial",Unit)
         foot = Imperial.unit(Length,'foot','ft')
-        qv3 = QuantityValue(x1,foot)
+        qv3 = ValueUnit(x1,foot)
         
         # When QVs use different systems of units,  
         # addition and subtraction is not permitted
         self.assertRaises(
             RuntimeError,
-            QuantityValue.__add__,
+            ValueUnit.__add__,
             qv1,
             qv3
         ) 
   
         self.assertRaises(
             RuntimeError,
-            QuantityValue.__sub__,
+            ValueUnit.__sub__,
             qv1,
             qv3
         ) 
