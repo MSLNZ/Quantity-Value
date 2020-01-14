a574848b70f9b5fd229ca9465daad5ee490667fa
Blair Hall <blair.hall@measurement.govt.nz>
2020-01-14 14:33:15 +1300

    result() renamed qresult and qratio added

--- quantity_value/quantity_value.py
+++ quantity_value/quantity_value.py
@@ -42,11 +42,11 @@ class ValueUnit(object):
             
         if not isinstance(lhs.unit,Unit):
             # Must resolve the unit before proceeding
-            lhs = ValueUnit( lhs.unit.multiplier*self.value, lhs.unit.reference_unit_for() ) 
+            lhs = ValueUnit( lhs.unit.multiplier*self.value, lhs.unit.reference_unit() ) 
         
         if not isinstance(rhs.unit,Unit):
             # Must resolve the unit before proceeding
-            rhs = ValueUnit( rhs.unit.multiplier*rhs.value, rhs.unit.reference_unit_for() ) 
+            rhs = ValueUnit( rhs.unit.multiplier*rhs.value, rhs.unit.reference_unit() ) 
             
         if lhs.unit is rhs.unit:
             return ValueUnit( lhs.value + rhs.value, lhs.unit )
@@ -81,11 +81,11 @@ class ValueUnit(object):
             
         if not isinstance(lhs.unit,Unit):
             # Must resolve the unit before proceeding
-            lhs = ValueUnit( lhs.unit.multiplier*self.value, lhs.unit.reference_unit_for() ) 
+            lhs = ValueUnit( lhs.unit.multiplier*self.value, lhs.unit.reference_unit() ) 
         
         if not isinstance(rhs.unit,Unit):
             # Must resolve the unit before proceeding
-            rhs = ValueUnit( rhs.unit.multiplier*rhs.value, rhs.unit.reference_unit_for() ) 
+            rhs = ValueUnit( rhs.unit.multiplier*rhs.value, rhs.unit.reference_unit() ) 
         
         if lhs.unit is rhs.unit:
             return ValueUnit( lhs.value - rhs.value, lhs.unit )
@@ -165,6 +165,31 @@ class ValueUnit(object):
             rhs.unit.system.unity / rhs.unit
         )
         
+    # def __floordiv__(self,rhs):
+        # lhs = self 
+        # if hasattr(rhs,'unit'):          
+            # if not lhs.unit.system is rhs.unit.system:
+                # raise RuntimeError("operation requires the same unit system")
+
+            # return ValueUnit(
+                # lhs.value / rhs.value,
+                # lhs.unit // rhs.unit
+            # )
+        # else:
+            # # Assume that the `rhs` behaves as a number 
+            # return ValueUnit(
+                # lhs.value / rhs, 
+                # lhs.unit // lhs.unit.system.unity 
+            # )
+
+    # def __rfloordiv__(self,lhs):
+        # rhs = self 
+        # # Assume that the `lhs` behaves as a number 
+        # return ValueUnit(
+            # lhs.value / rhs.value, 
+            # rhs.unit.system.unity // rhs.unit   
+        # )
+        
     # # Python 2.x backward compatibility
     # def __div__(self,rhs):
         # return ValueUnit.__truediv__(self,rhs)
@@ -197,22 +222,51 @@ def unit(vu):
         return None 
        
 #----------------------------------------------------------------------------
-def result(value_unit, unit=None, result_x_fn=lambda x,*arg,**kwarg:x, *arg,**kwarg):
+def qresult(
+    value_unit, 
+    unit=None, 
+    simplify=True, 
+    value_result = lambda x, *arg, **kwarg: x, 
+    *arg,
+    **kwarg
+    ):
     """
     Return a `qvalue`.
     
-    If no `unit` is given the measure will be converted to the reference 
-    unit of the unit system. Otherwise the measure will be converted to
-    the `unit`.
+    `unit` may be the name of a unit, or a unit object. The measure will be 
+    converted to this unit.
     
-    A value result function `result_x_fn` can be supplied 
+    `simplify` controls when the unit dimensions will be simplified.
+    
+    If no `unit` is given the measure will be converted to the reference 
+    unit of the system. 
+    
+    A function `value_result` can be supplied 
     to apply a final processing to the value.
     """
     if unit:
-        # Use the preferred unit 
+        # Use a preferred unit 
+
+        if isinstance(unit,str):
+            # `unit` is the name of a unit, so look up the object
+            unit = value_unit.unit.system[unit]
+            
+        if simplify:
+            ref_unit = value_unit.unit.simplify().reference_unit() 
+        else:
+            ref_unit = value_unit.unit.reference_unit()
+                
+        if unit.kind_of_quantity != ref_unit.kind_of_quantity:
+            raise RuntimeError(
+                "{} are incompatible with {}".format(
+                    unit,
+                    ref_unit
+                )
+            )
+            
         multiplier = value_unit.unit.multiplier/unit.multiplier
         return ValueUnit( 
-            result_x_fn(
+            value_result(
                 value_unit.value*multiplier , 
                 *arg, 
                 **kwarg
@@ -220,16 +274,68 @@ def result(value_unit, unit=None, result_x_fn=lambda x,*arg,**kwarg:x, *arg,**kw
             unit 
         )
     else:
+        if simplify:
+            unit = value_unit.unit.simplify().reference_unit() 
+        else:
+            unit = value_unit.unit.reference_unit()
+            
         return ValueUnit( 
-            result_x_fn(
+            value_result(
                 value_unit.unit.multiplier*value_unit.value, 
                 *arg, 
                 **kwarg
             ), 
-            value_unit.unit.reference_unit_for() 
+            unit  
         )
         
-             
+#----------------------------------------------------------------------------
+def qratio(value_unit_1, value_unit_2, unit=None ):
+    """
+    Return a quantity-value for the ratio keeping the dimensions 
+    of the numerator and denominator separate.
 
+    If no `unit` is given the ratio will be converted to the reference 
+    unit of the system. 
+
+    """
+    # Return a qvalue with units that are appropriate for the ratio
+    
+    koq_1 = value_unit_1.unit.reference_unit().kind_of_quantity 
+    koq_2 = value_unit_2.unit.reference_unit().kind_of_quantity 
+    
+    if koq_1 != koq_2:
+        raise RuntimeError(
+            "Different kinds of quantity: {} and {}".format(
+                koq_1,koq_2
+            )
+        )
+    
+    if unit and koq_1 != unit.kind_of_quantity:
+        raise RuntimeError(
+            "Different kinds of quantity: {} and {}".format(
+                koq_1, unit.kind_of_quantity
+            )
+        )
+   
+    if unit:
+        value = (
+            value_unit_1.unit.multiplier*value_unit_1.value /
+            (value_unit_2.unit.multiplier*value_unit_2.value) 
+        )/unit.multiplier
+        
+        return ValueUnit( 
+            value_unit.unit.multiplier*value_unit.value, 
+            unit
+        )    
+    else:
+        value = (
+            value_unit_1.unit.multiplier*value_unit_1.value /
+            (value_unit_2.unit.multiplier*value_unit_2.value) 
+        )
+
+        return ValueUnit( 
+            value, 
+            (value_unit_1.unit//value_unit_2.unit).reference_unit()
+        )
     
     
